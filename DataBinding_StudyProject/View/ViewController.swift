cbef0fe82138f5b0bc9165d98cfd4996350dc594
KimWooSun <kipsong133@gmail.com>
2021-10-06 19:06:10 +0900

    더미데이터 추가

--- DataBinding_StudyProject/View/ViewController.swift
+++ DataBinding_StudyProject/View/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // data
+    var dummyIntData = Array(10...20)
+    
     // UI
     @IBOutlet weak var tableView: UITableView!
     
@@ -25,11 +28,12 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        dummyIntData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(dummyIntData[indexPath.row]) 번 째 데이터 입니다. ^_^"
         return cell
     }
 }
