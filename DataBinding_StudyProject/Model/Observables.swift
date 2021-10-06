8f5051a5bf5b4481a5f45f6dd2979d93e6e8b47e
KimWooSun <kipsong133@gmail.com>
2021-10-06 18:47:05 +0900

    옵저버블 클래스 구현

---
+++ DataBinding_StudyProject/Model/Observables.swift
@@ -0,0 +1,28 @@
+//
+//  Observables.swift
+//  DataBinding_StudyProject
+//
+//  Created by 김우성 on 2021/10/06.
+//
+
+import Foundation
+
+class Observables<T> {
+    private var listener: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ closure: @escaping (T) -> Void) {
+        closure(value)
+        listener = closure
+    }
+}
+
