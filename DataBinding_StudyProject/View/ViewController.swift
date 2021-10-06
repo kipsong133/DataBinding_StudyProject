0bd267bad7cac166c3f4215691af15830dedf27f
KimWooSun <kipsong133@gmail.com>
2021-10-06 19:10:04 +0900

    데이터 모델의 변경을 위해 action 메소드 구현

--- DataBinding_StudyProject/View/ViewController.swift
+++ DataBinding_StudyProject/View/ViewController.swift
@@ -9,23 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // data
+    // MARK: - Properties
     var dummyIntData = Array(10...20)
     
     // UI
     @IBOutlet weak var tableView: UITableView!
     
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTableView()
     }
     
+    
+    // MARK: - Actions
+    @IBAction func addButtonDidTap(_ sender: UIBarButtonItem) {
+        /* 새로운 데이터를 추가합니다. */
+        let new = Int.random(in: 100...200)
+        dummyIntData.append(new)
+    }
+    
+    
+    // MARK: - Helpers
     private func setUpTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        
     }
 }
 
+
+// MARK: - UITableView Protocol Method
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         dummyIntData.count
