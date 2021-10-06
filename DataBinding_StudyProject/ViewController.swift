688896b6d7791f0d6f2909e0ce5a4e562b2b7907
KimWooSun <kipsong133@gmail.com>
2021-10-06 18:44:07 +0900

    Initial Commit

---
+++ DataBinding_StudyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataBinding_StudyProject
+//
+//  Created by 김우성 on 2021/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
